7177c8175eca76c5fde4ca3fc6625ae4d6766ed6
Vladimir <kail-vladimir@yandex.ru>
2022-01-20 18:30:13 +0300

    start commit

---
+++ src/test/java/com/vladimir/questionnairesystem/QuestionnaireSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimir.questionnairesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
